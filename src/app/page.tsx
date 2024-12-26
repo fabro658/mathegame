a5639090424779ca4bf07de5532c8dff4bae5db4
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-26 18:47:25 -0500

    Ajout menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,27 +33,27 @@ export default function Home() {
           <span className="text-xl font-bold">MathGame</span>
         </div>
         <div className="flex flex-col gap-4 absolute right-8 top-1/2 transform -translate-y-1/2">
-          <Link href="/Niveau/niveau1">
+          <Link href="/Niveau/niveau1.tsx">
             <a className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
               Niveau 1
             </a>
           </Link>
-          <Link href="/Niveau/page2">
+          <Link href="/Niveau/niveau2.tsx">
             <a className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
             Niveau 2
             </a>
           </Link>
-          <Link href="/Niveau/page3">
+          <Link href="/Niveau/niveau3.tsx">
             <a className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
             Niveau 3
             </a>
           </Link>
-          <Link href="/Niveau/page4">
+          <Link href="/Niveau/niveau4.tsx">
             <a className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
             Niveau 4
             </a>
           </Link>
-          <Link href="/Niveau/page5">
+          <Link href="/Niveau/niveau5.tsx">
             <a className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
             Niveau 5
             </a>
