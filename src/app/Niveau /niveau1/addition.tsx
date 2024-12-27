ec91a627e94fc2f7dc50b086956e9e2e3b0d8fd5
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-27 13:43:19 -0500

    Correction styles

--- src/app/Niveau /niveau1/addition.tsx
+++ src/app/Niveau /niveau1/addition.tsx
@@ -1,9 +1,8 @@
 export default function Addition() {
-    return (
-      <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
-        <h1 className="text-3xl font-bold mb-4">Addition</h1>
-        <p>Bienvenue sur la page d'Addition !</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
+      <h1 className="text-3xl font-bold mb-4">Addition</h1>
+      <p>Bienvenue sur la page d&apos;Addition !</p>
+    </div>
+  );
+}
--- src/app/Niveau /niveau1/division.tsx
+++ src/app/Niveau /niveau1/division.tsx
@@ -1,9 +1,8 @@
 export default function division() {
-    return (
-      <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
-        <h1 className="text-3xl font-bold mb-4">division</h1>
-        <p>Bienvenue sur la page d'division !</p>
-      </div>
-    );
-  }
-  
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
+      <h1 className="text-3xl font-bold mb-4">division</h1>
+      <p>Bienvenue sur la page d&apos;division !</p>
+    </div>
+  );
+}
