da27e54e880133b75c7c5811171e19a7a77c5cda
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-28 15:21:34 -0500

    Ajout page n1

---
+++ src/app/niveaux/niveau1/addition/page.tsx
@@ -0,0 +1,8 @@
+// src/app/page1.tsx
+export default function Addition() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold">Addition</h1>
+    </div>
+  );
+}
---
+++ src/app/niveaux/niveau1/fraction/page.tsx
@@ -0,0 +1,8 @@
+// src/app/page1.tsx
+export default function Fraction() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <h1 className="text-2xl font-bold">Fraction</h1>
+    </div>
+  );
+}
