46ecc9d0f0434f3fa27828638297a628efd9a76d
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-28 13:27:20 -0500

    Niveau4

---
+++ src/app/niveaux/niveau4/algebre.tsx
@@ -0,0 +1,8 @@
+export default function algebre() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
+      <h1 className="text-3xl font-bold mb-4">Algèbre</h1>
+      <p>Bienvenue sur la page d&apos;algèbre !</p>
+    </div>
+  );
+}
