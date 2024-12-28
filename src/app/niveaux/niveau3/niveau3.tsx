f74380696869aa978711e1c86939186c16643383
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-28 10:36:12 -0500

    Ajout niveau 4

--- src/app/niveaux/niveau3/niveau3.tsx
+++
@@ -1,8 +0,0 @@
-// src/app/page1.tsx
-export default function page3() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold">Page 3</h1>
-    </div>
-  );
-}
---
+++ src/app/niveaux/niveau3/page.tsx
@@ -0,0 +1,21 @@
+export default function Page() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
+      <h1 className="text-4xl font-bold mb-12">Niveau 3 - Choisissez une opération</h1>
+      <div className="grid grid-cols-2 gap-8">
+        <a
+          href="/niveaux/niveau2/multiplication"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
+        >
+          Périmètre 
+        </a>
+        <a
+          href="/niveaux/niveau2/division"
+          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
+        >
+          Aire
+        </a>
+      </div>
+    </div>
+  );
+}
