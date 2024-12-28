041c05532f8b427fb24dc949e03ba1a7ec5f52b4
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-28 13:44:16 -0500

    Ajout niveua4

--- src/app/niveaux/niveau4/page.tsx
+++ src/app/niveaux/niveau4/page.tsx
@@ -1,8 +1,17 @@
-// src/app/page1.tsx
-export default function page4() {
+import Link from 'next/link'; // Si vous utilisez Next.js
+
+export default function Page() {
   return (
-    <div className="flex items-center justify-center min-h-screen">
-      <h1 className="text-2xl font-bold">Page 4</h1>
-    </div>
-  );
-}
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
+      <h1 className="text-4xl font-bold mb-12">Niveau 4 - Choisissez une opération</h1>
+      <div className="grid grid-cols-2 gap-8">
+        <Link
+          href="/niveaux/niveaue/algebre"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
+          >
+          Algèbre
+          </Link>
+        </div>
+      </div>
+    );
+  }
