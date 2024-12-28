3030ba2a5293d50c0a1d7c2c491897610e524c28
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-28 10:13:29 -0500

    Grossir bouton niveau1

--- src/app/niveaux/niveau1/page.tsx
+++ src/app/niveaux/niveau1/page.tsx
@@ -3,16 +3,16 @@ export default function Page() {
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
       <h1 className="text-3xl font-bold mb-8">Niveau 1 - Choisissez une opération</h1>
       <div className="grid grid-cols-2 gap-4">
-        <a href="/Niveau/niveau1/addition" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center">
+        <a href="/niveaux/niveau1/addition" className="bg-blue-500 hover:bg-blue-700 text-white font-bold h-12 w-32 rounded text-center flex items-center justify-center">
           Addition
         </a>
-        <a href="/Niveau/niveau1/soustraction" className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded text-center">
+        <a href="/niveaux/niveau1/soustraction" className="bg-red-500 hover:bg-red-700 text-white font-bold h-12 w-32 rounded text-center flex items-center justify-center">
           Soustraction
         </a>
-        <a href="/Niveau/niveau1/multiplication" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded text-center">
+        <a href="/niveaux/niveau1/multiplication" className="bg-green-500 hover:bg-green-700 text-white font-bold h-12 w-32 rounded text-center flex items-center justify-center">
           Multiplication
         </a>
-        <a href="/Niveau/niveau1/division" className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded text-center">
+        <a href="/niveaux/niveau1/division" className="bg-purple-500 hover:bg-purple-700 text-white font-bold h-12 w-32 rounded text-center flex items-center justify-center">
           Division
         </a>
       </div>
