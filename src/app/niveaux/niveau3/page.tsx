c0af1248b7ef0732f9d76f1111a4ef13b4c5a1de
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-28 13:25:16 -0500

    Ajout niveau3

--- src/app/niveaux/niveau3/page.tsx
+++ src/app/niveaux/niveau3/page.tsx
@@ -4,16 +4,22 @@ export default function Page() {
       <h1 className="text-4xl font-bold mb-12">Niveau 3 - Choisissez une opération</h1>
       <div className="grid grid-cols-2 gap-8">
         <a
-          href="/niveaux/niveau2/multiplication"
+          href="/niveaux/niveau3/perimetre"
           className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
         >
           Périmètre 
         </a>
         <a
-          href="/niveaux/niveau2/division"
+          href="/niveaux/niveau3/aire"
           className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
         >
           Aire
+        </a>
+        <a
+        href="/niveaux/niveau3/priooperation"
+          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
+        >
+          Priorité d'opération
         </a>
       </div>
     </div>
