6478b331ee771a0b341db2c225f4c5d19296e3ef
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-29 10:10:33 -0500

    niveau2

--- src/app/niveaux/niveau2/page.tsx
+++ src/app/niveaux/niveau2/page.tsx
@@ -1,6 +1,8 @@
 import Link from 'next/link'; 
 
 export default function Page() {
+  const buttonClass = "bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]";
+
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
       <h1 className="text-4xl font-bold mb-12">Niveau 2 - Choisissez une opération</h1>
@@ -8,22 +10,32 @@ export default function Page() {
         <div className="flex gap-8">
           <Link
             href="/niveaux/niveau2/multiplication"
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+            className={`${buttonClass} bg-green-500`}
+            aria-label="Page de multiplication"
           >
             Multiplication
           </Link>
           <Link
             href="/niveaux/niveau2/division"
-            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+            className={`${buttonClass} bg-purple-500`}
+            aria-label="Page de division"
           >
             Division
           </Link>
         </div>
         <Link
-          href="/niveaux/niveau2/fraction"
-          className="bg-orange-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+          href="/niveaux/niveau2/multiplicationfraction"
+          className={`${buttonClass} bg-orange-500`}
+          aria-label="Page de multiplication de fraction"
         >
-          Fraction
+          Multiplication de fraction
+        </Link>
+        <Link
+          href="/niveaux/niveau2/divisionfraction"
+          className={`${buttonClass} bg-orange-500`}
+          aria-label="Page de division de fraction"
+        >
+          Division de fraction
         </Link>
       </div>
     </div>
