1841e63c1e55edabcb44f07c1b9d5ec2e556c278
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-29 10:47:34 -0500

    Perimetre

--- src/app/niveaux/niveau3/perimetre/page.tsx
+++ src/app/niveaux/niveau3/perimetre/page.tsx
@@ -95,9 +95,9 @@ export default function Perimetre() {
 
       {!isValidated && (
         <>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+          <div className="grid grid-cols-1 sm:grid-cols-3 gap-4">
             {questions.slice(currentPage * questionsPerPage, (currentPage + 1) * questionsPerPage).map(({ questionText }, index) => (
-              <div key={index} className="flex items-center gap-2">
+              <div key={index} className="flex flex-col items-start gap-2">
                 <div className="font-bold text-black">{questionText}</div>
                 <input
                   type="number"
