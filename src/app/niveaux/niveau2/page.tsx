abe4e59177c061c4001230d0ec6e157606eda5bd
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-31 09:08:51 -0500

    new

--- src/app/niveaux/niveau2/page.tsx
+++ src/app/niveaux/niveau2/page.tsx
@@ -48,7 +48,7 @@ export default function Page() {
           </Link> 
           {/* Bouton centré en dessous */}
           <Link
-          href="/niveaux/niveau1/revision"
+          href="/niveaux/niveau2/revision"
           className="bg-yellow-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
         >
           Révision
