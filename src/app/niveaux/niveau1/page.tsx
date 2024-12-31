6f841a412225c38486fa5d19d853fc92d4ace63a
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-30 21:27:43 -0500

    essai

--- src/app/niveaux/niveau1/page.tsx
+++ src/app/niveaux/niveau1/page.tsx
@@ -1,23 +1,17 @@
 "use client";
 
-import { useState } from "react";
 import Link from "next/link";
 
 export default function Page() {
-  // État pour gérer la visibilité du bouton de retour
-  const [showReturnButton, setShowReturnButton] = useState(true);
-
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900 relative">
       {/* Bouton de retour en haut à droite */}
-      {showReturnButton && (
-        <Link
-          href="/"
-          className="absolute top-4 right-4 bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full shadow-lg"
-        >
-          Retour
-        </Link>
-      )}
+      <Link
+        href="/"
+        className="absolute top-4 right-4 bg-gray-800 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full shadow-lg"
+      >
+        Retour
+      </Link>
 
       <h1 className="text-4xl font-bold mb-12">Niveau 1 - Choisissez une opération</h1>
       <div className="flex flex-col items-center gap-8">
