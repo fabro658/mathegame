8e34a139cd343b5bb6424ffbcbdc024ef7b78aef
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-30 20:59:36 -0500

    new

--- src/app/niveaux/niveau2/page.tsx
+++ src/app/niveaux/niveau2/page.tsx
@@ -1,210 +1,45 @@
-"use client";
+import Link from 'next/link'; 
 
-import { useState } from "react";
-import Link from "next/link";
-
-export default function Multiplication() {
-  const totalQuestions = 36;
-  const questionsPerPage = 6; // 3 colonnes x 2 lignes
-  const [answers, setAnswers] = useState<(number | null)[]>(Array(totalQuestions).fill(null));
-  const [isValidated, setIsValidated] = useState(false);
-  const [hasPassed, setHasPassed] = useState(false);
-  const [currentPage, setCurrentPage] = useState(0);
-
-  // Génération des questions de multiplication
-  const questions = Array.from({ length: totalQuestions }, () => {
-    const factor1 = Math.floor(Math.random() * 10) + 1;
-    const factor2 = Math.floor(Math.random() * 10) + 1;
-    return [factor1, factor2];
-  });
-
-  const correctAnswers = questions.map(([factor1, factor2]) => factor1 * factor2); // Réponses correctes
-
-  // Calculer le pourcentage de réponses complétées
-  const completedAnswers = answers.filter((answer) => answer !== null).length;
-  const completionPercentage = Math.round((completedAnswers / totalQuestions) * 100);
-
-  const handleChange = (index: number, value: string) => {
-    const newAnswers = [...answers];
-    const parsedValue = parseFloat(value);
-    newAnswers[index] = isNaN(parsedValue) ? null : parsedValue;
-    setAnswers(newAnswers);
-  };
-
-  const handleValidation = () => {
-    const startIndex = currentPage * questionsPerPage;
-    const endIndex = startIndex + questionsPerPage;
-    const pageAnswers = answers.slice(startIndex, endIndex);
-
-    // Vérification si toutes les réponses sont remplies
-    if (pageAnswers.includes(null)) {
-      alert("Veuillez remplir toutes les réponses sur cette page avant de valider.");
-      return;
-    }
-
-    // Validation des réponses
-    const newAnswers = [...answers];
-    let allCorrect = true;
-
-    pageAnswers.forEach((answer, index) => {
-      const globalIndex = startIndex + index;
-      if (answer !== correctAnswers[globalIndex]) {
-        allCorrect = false;
-        newAnswers[globalIndex] = null; // Réinitialiser uniquement les mauvaises réponses
-      }
-    });
-
-    setAnswers(newAnswers);
-    setIsValidated(true);
-    setHasPassed(allCorrect);
-  };
-
-  const handleNextPage = () => {
-    if (currentPage < Math.floor(totalQuestions / questionsPerPage) - 1) {
-      setCurrentPage(currentPage + 1);
-      setIsValidated(false); // Réinitialiser la validation pour la page suivante
-      setHasPassed(false); // Réinitialiser l'état de réussite pour la page suivante
-    }
-  };
-
-  const handlePreviousPage = () => {
-    if (currentPage > 0) {
-      setCurrentPage(currentPage - 1);
-      setIsValidated(false); // Réinitialiser la validation pour revenir à la page précédente
-      setHasPassed(false); // Réinitialiser l'état de réussite pour la page précédente
-    }
-  };
-
-  // Calcul pour dessiner le cercle de progression
-  const radius = 50; // Ajustez le rayon pour qu'il soit égal à celui de l'addition
-  const circumference = 2 * Math.PI * radius;
-  const offset = circumference - (completionPercentage / 100) * circumference;
+export default function Page() {
+  const buttonClass = "bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]";
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-black relative">
-      {/* Bouton pour naviguer vers la page "Apprendre" */}
-      <Link
-        href="/menu/apprendre"
-        className="absolute top-4 right-4 bg-orange-500 text-white py-2 px-4 rounded font-bold"
-      >
-        Apprendre
-      </Link>
-
-      {/* cercle de progression */}
-      <div className="absolute top-4 left-4 w-32 h-32">
-        <svg className="transform -rotate-90" width="100%" height="100%">
-          <circle
-            cx="50%"
-            cy="50%"
-            r={radius}
-            fill="none"
-            stroke="#e5e5e5"
-            strokeWidth="4"
-          />
-          <circle
-            cx="50%"
-            cy="50%"
-            r={radius}
-            fill="none"
-            stroke="#3b82f6"
-            strokeWidth="4"
-            strokeDasharray={circumference}
-            strokeDashoffset={circumference - (circumference * completionPercentage) / 100}
-            className="transition-all duration-500"
-          />
-        </svg>
-        <div className="absolute inset-0 flex items-center justify-center">
-          <span className="text-xl font-bold text-blue-500">{completionPercentage}%</span>
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
+      <h1 className="text-4xl font-bold mb-12">Niveau 2 - Choisissez une opération</h1>
+      <div className="flex flex-col items-center gap-8">
+        <div className="flex gap-8">
+          <Link
+            href="/niveaux/niveau2/multiplication"
+            className={`${buttonClass} bg-green-500`}
+            aria-label="Page de multiplication"
+          >
+            Multiplication
+          </Link>
+          <Link
+            href="/niveaux/niveau2/division"
+            className={`${buttonClass} bg-purple-500`}
+            aria-label="Page de division"
+          >
+            Division
+          </Link>
+        </div>
+        <div className="flex gap-8">
+          <Link
+            href="/niveaux/niveau2/multiplicationfraction"
+            className={`${buttonClass} bg-orange-500`}
+            aria-label="Page de multiplication de fraction"
+          >
+            Multiplication de fraction
+          </Link>
+          <Link
+            href="/niveaux/niveau2/divisionfraction"
+            className={`${buttonClass} bg-red-500`}
+            aria-label="Page de division de fraction"
+          >
+            Division de fraction
+          </Link>
         </div>
       </div>
-
-      <h1 className="text-3xl font-bold mb-6">Multiplication</h1>
-
-      {/* Questions de la page actuelle */}
-      {!isValidated && (
-        <>
-          <div className="grid grid-cols-3 gap-6">
-            {questions
-              .slice(currentPage * questionsPerPage, (currentPage + 1) * questionsPerPage)
-              .map(([factor1, factor2], index) => (
-                <div key={index} className="flex items-center gap-4">
-                  <button className="bg-blue-500 text-white font-bold py-6 px-8 rounded w-full" disabled>
-                    {factor1} × {factor2}
-                  </button>
-                  <input
-                    type="number"
-                    className="border border-gray-400 p-4 rounded w-full text-center text-black"
-                    value={answers[currentPage * questionsPerPage + index] || ""}
-                    onChange={(e) => handleChange(currentPage * questionsPerPage + index, e.target.value)}
-                    style={{ appearance: 'none' }} // Enlève les petites flèches du champ input
-                  />
-                </div>
-              ))}
-          </div>
-
-          {/* Boutons de validation et de navigation */}
-          <div className="mt-6 flex gap-4">
-            {currentPage > 0 && (
-              <button
-                className="bg-gray-500 text-white py-2 px-6 rounded font-bold"
-                onClick={handlePreviousPage}
-              >
-                Précédent
-              </button>
-            )}
-            <button
-              onClick={handleValidation}
-              className="bg-blue-500 text-white py-2 px-6 rounded font-bold"
-            >
-              Valider les réponses
-            </button>
-            {isValidated && hasPassed && currentPage < Math.floor(totalQuestions / questionsPerPage) - 1 && (
-              <button
-                className="bg-blue-500 text-white py-2 px-6 rounded font-bold"
-                onClick={handleNextPage}
-              >
-                Suivant
-              </button>
-            )}
-          </div>
-        </>
-      )}
-
-      {/* Résultats après validation */}
-      {isValidated && (
-        <>
-          {hasPassed ? (
-            <div>
-              <p className="text-green-600 font-bold text-xl">Bravo ! Toutes vos réponses sont correctes.</p>
-              {currentPage < Math.floor(totalQuestions / questionsPerPage) - 1 ? (
-                <button
-                  className="mt-6 bg-blue-500 text-white py-2 px-6 rounded font-bold"
-                  onClick={handleNextPage}
-                >
-                  Passer à la série suivante
-                </button>
-              ) : (
-                <button
-                  className="mt-6 bg-blue-500 text-white py-2 px-6 rounded font-bold"
-                  onClick={() => alert("Vous avez complété toutes les questions !")}
-                >
-                  Terminer
-                </button>
-              )}
-            </div>
-          ) : (
-            <div>
-              <p className="text-red-600 font-bold text-xl">Certaines réponses sont incorrectes. Corrigez-les.</p>
-              <button
-                className="mt-6 bg-gray-500 text-white py-2 px-6 rounded font-bold"
-                onClick={() => setIsValidated(false)}
-              >
-                Revenir pour corriger
-              </button>
-            </div>
-          )}
-        </>
-      )}
     </div>
   );
 }
