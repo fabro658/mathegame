dd33196aff94fcb7d7b40d41ee7ad65b16d3880b
felix <felix-antoine.brodeur.1@ulaval.ca>
2024-12-31 09:05:13 -0500

    new

--- src/app/niveaux/niveau3/perimetre/page.tsx
+++ src/app/niveaux/niveau3/perimetre/page.tsx
@@ -94,7 +94,7 @@ export default function Perimetre() {
         Apprendre
       </Link>
       <Link
-        href="/niveaux/niveau2"
+        href="/niveaux/niveau3"
         className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
       >
         Retour
