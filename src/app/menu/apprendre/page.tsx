c2602202d29956c4f02cd8af9963137fbe21c3bd
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-01 18:21:22 -0500

    apprendre

--- src/app/menu/apprendre/page.tsx
+++ src/app/menu/apprendre/page.tsx
@@ -6,39 +6,39 @@ export default function Page() {
       </h1>
       <div className="flex flex-col gap-4 text-lg w-full pl-0">
         {/* Menu complètement collé à gauche */}
-        <a href="/menu/addition" 
+        <a href="/menu/apprendre/addition" 
         className="hover:underline text-gray-700">
           Addition
         </a>
-        <a href="/menu/soustraction" 
+        <a href="/menu/apprendre/soustraction" 
         className="hover:underline text-gray-700">
           Soustraction
         </a>
-        <a href="/menu/multiplication" 
+        <a href="/menu/apprendre/multiplication" 
         className="hover:underline text-gray-700">
           Multiplication
         </a>
-        <a href="/menu/division" 
+        <a href="/menu/apprendre/division" 
         className="hover:underline text-gray-700">
           Division
         </a>
-        <a href="/menu/fraction"
+        <a href="/menu/apprendre/fraction"
         className="hover:underline text-gray-700">
           Fraction
         </a>
-        <a href="/menu/aire" 
+        <a href="/menu/apprendre/aire" 
         className="hover:underline text-gray-700">
           Aire
         </a>
-        <a href="/menu/perimetre"
+        <a href="/menu/apprendre/perimetre"
         className="hover:underline text-gray-700">
           Périmètre
         </a>
-        <a href="/menu/algebre" 
+        <a href="/menu/apprendre/algebre" 
         className="hover:underline text-gray-700">
           Algèbre
         </a>
-        <a href="/menu/priorite operation" 
+        <a href="/menu/apprendre/priorite operation" 
         className="hover:underline text-gray-700">
           Priorité d&apos;opération
         </a>
