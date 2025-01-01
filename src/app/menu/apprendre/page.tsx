e0142e0586df69e3c625e554297d8e43ad79d91f
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-01 18:10:36 -0500

    menu

--- src/app/menu/apprendre/page.tsx
+++ src/app/menu/apprendre/page.tsx
@@ -6,31 +6,40 @@ export default function Page() {
       </h1>
       <div className="flex flex-col gap-4 text-lg w-full pl-0">
         {/* Menu complètement collé à gauche */}
-        <a href="#" className="hover:underline text-gray-700">
+        <a href="/menu/addition" 
+        className="hover:underline text-gray-700">
           Addition
         </a>
-        <a href="#" className="hover:underline text-gray-700">
+        <a href="/menu/soustraction" 
+        className="hover:underline text-gray-700">
           Soustraction
         </a>
-        <a href="#" className="hover:underline text-gray-700">
+        <a href="/menu/multiplication" 
+        className="hover:underline text-gray-700">
           Multiplication
         </a>
-        <a href="#" className="hover:underline text-gray-700">
+        <a href="/menu/division" 
+        className="hover:underline text-gray-700">
           Division
         </a>
-        <a href="#" className="hover:underline text-gray-700">
+        <a href="/menu/fraction"
+        className="hover:underline text-gray-700">
           Fraction
         </a>
-        <a href="#" className="hover:underline text-gray-700">
+        <a href="/menu/aire" 
+        className="hover:underline text-gray-700">
           Aire
         </a>
-        <a href="#" className="hover:underline text-gray-700">
+        <a href="/menu/perimetre"
+        className="hover:underline text-gray-700">
           Périmètre
         </a>
-        <a href="#" className="hover:underline text-gray-700">
+        <a href="/menu/algebre" 
+        className="hover:underline text-gray-700">
           Algèbre
         </a>
-        <a href="#" className="hover:underline text-gray-700">
+        <a href="/menu/priorite operation" 
+        className="hover:underline text-gray-700">
           Priorité d&apos;opération
         </a>
       </div>
