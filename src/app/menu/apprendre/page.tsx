37196dc77d3bc39e85a7204186eba0eb2a9e639b
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-01 19:48:07 -0500

    apprendre

--- src/app/menu/apprendre/page.tsx
+++ src/app/menu/apprendre/page.tsx
@@ -1,38 +1,29 @@
-"use client";
-
-import Link from "next/link";
+// src/app/menu/apprendre/page.tsx
 
 export default function Page() {
   return (
-    <div className="min-h-screen flex flex-col justify-start items-center bg-gray-100 text-gray-900">
-      <h1 className="text-4xl font-bold mb-12 text-center underline">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
+      <h1 className="text-4xl font-bold mb-12 underline">
         Zone d&apos;apprentissage
       </h1>
-
       <div className="flex flex-col gap-4 text-lg w-full pl-0">
         {/* Menu complètement collé à gauche */}
-        <a href="/menu/apprendre/opérations arithmétiques"
-        className="hover:underline text-gray-700">
-          Opérations arithmétiques
+        <a href="/menu/apprendre/opérations arithmétiques" className="hover:underline text-gray-700">
+          opérations arithmétiques
         </a>
-        <a href="/menu/apprendre/fraction"
-        className="hover:underline text-gray-700">
+        <a href="/menu/apprendre/fraction" className="hover:underline text-gray-700">
           Fraction
         </a>
-        <a href="/menu/apprendre/aire" 
-        className="hover:underline text-gray-700">
+        <a href="/menu/apprendre/aire" className="hover:underline text-gray-700">
           Aire
         </a>
-        <a href="/menu/apprendre/perimetre"
-        className="hover:underline text-gray-700">
+        <a href="/menu/apprendre/perimetre" className="hover:underline text-gray-700">
           Périmètre
         </a>
-        <a href="/menu/apprendre/algebre" 
-        className="hover:underline text-gray-700">
+        <a href="/menu/apprendre/algebre" className="hover:underline text-gray-700">
           Algèbre
         </a>
-        <a href="/menu/apprendre/priorite operation" 
-        className="hover:underline text-gray-700">
+        <a href="/menu/apprendre/priorite operation" className="hover:underline text-gray-700">
           Priorité d&apos;opération
         </a>
       </div>
