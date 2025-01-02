8284758aaac155df22924a425689806e13cecb4f
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-01 20:26:40 -0500

    new

--- src/app/niveaux/niveau3/page.tsx
+++ src/app/niveaux/niveau3/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
         <div className="flex gap-8">
           <Link
             href="/niveaux/niveau3/perimetre"
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
           >
             Périmètre
           </Link>
@@ -20,15 +20,15 @@ export default function Page() {
       </Link>
           <Link
             href="/niveaux/niveau3/aire"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
           >
             Aire
           </Link>
         </div>
         <Link
           href="/niveaux/niveau3/Géometrie"
-          className="bg-red-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
-        >
+          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+          >
           Géométrie
         </Link>
       </div>
