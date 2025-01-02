581942653f01e8beb2d4654b13d24a04d554db01
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-01 20:01:24 -0500

    new

--- src/app/menu/apprendre/page.tsx
+++ src/app/menu/apprendre/page.tsx
@@ -5,6 +5,14 @@ import Link from "next/link";
 export default function Page() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
+      {/* Bouton Retour à l'accueil */}
+      <Link
+        href="/"
+        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
+      >
+        Retour
+      </Link>
+
       <h1 className="text-4xl font-bold mb-12 underline">Zone d&apos;apprentissage</h1>
       <div className="flex flex-col gap-4 text-lg w-full pl-0">
         {/* Menu complètement collé à gauche */}
