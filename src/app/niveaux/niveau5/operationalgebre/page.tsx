c6245143f9000ba8556accafaee8591feb529b21
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-03 14:52:41 -0500

    n5

--- src/app/niveaux/niveau5/operationalgebre/page.tsx
+++ src/app/niveaux/niveau5/operationalgebre/page.tsx
@@ -1,8 +1,8 @@
-export default function Algebre() {
+export default function Operationalgebrique() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
-      <h1 className="text-3xl font-bold mb-4">Algèbre</h1>
-      <p>Bienvenue sur la page d&apos;algèbre !</p>
+      <h1 className="text-3xl font-bold mb-4">Opération Algébrique</h1>
+      <p>Bienvenue sur la page des opération alébrique !</p>
     </div>
   );
 }
