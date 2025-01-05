ae7a395d0682030a3f1017fadf27ce6394eca0e1
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-05 12:36:33 -0500

    Bouton retour

--- src/app/niveaux/niveau3/geometrie/page.tsx
+++ src/app/niveaux/niveau3/geometrie/page.tsx
@@ -122,7 +122,7 @@ export default function GeometryPractice() {
         Apprendre
       </Link>
       <Link
-        href="/niveaux/niveau4"
+        href="/niveaux/niveau3"
         className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
       >
         Retour
