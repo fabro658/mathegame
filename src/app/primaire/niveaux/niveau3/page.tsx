2d1e5bc701a2aa5a52ed3bec1c9e2828c8ef2153
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-07 15:41:33 -0500

    new

--- src/app/primaire/niveaux/niveau3/page.tsx
+++ src/app/primaire/niveaux/niveau3/page.tsx
@@ -3,19 +3,20 @@ import Link from 'next/link'; // Si vous utilisez Next.js
 export default function Page() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
-      <h1 className="text-4xl font-bold mb-12">Niveau 3  - Les fractions</h1>
+      <h1 className="text-4xl font-bold mb-12">Niveau 3 - Les fractions</h1>
       <div className="flex flex-col items-center gap-8">
         <div className="flex gap-8">
-        <Link
-        href="/"
-        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
-      >
-        Retour
-      </Link><Link
+          <Link
+            href="/"
+            className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
+          >
+            Retour
+          </Link>
+          <Link
             href="/niveaux/niveau3/additionfraction"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
           >
-            Addition de de fraction
+            Addition de fraction
           </Link>
           <Link
             href="/niveaux/niveau3/soustractionfraction"
@@ -27,15 +28,15 @@ export default function Page() {
         {/* Deuxième ligne avec 2 boutons */}
         <div className="flex justify-center gap-8">
           <Link
-            href="/niveaux/niveau2/multiplicationfraction"
+            href="/niveaux/niveau3/multiplicationfraction"
             className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
             aria-label="Page de multiplication de fraction"
           >
             Multiplication de fraction
           </Link>
           <Link
-            href="/niveaux/niveau2/divisionfraction"
-            className="bg-green-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+            href="/niveaux/niveau3/divisionfraction"
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
             aria-label="Page de division de fraction"
           >
             Division de fraction
@@ -43,8 +44,8 @@ export default function Page() {
         </div>
         <Link
           href="/niveaux/niveau3/comparer_fraction"
-          className="bg-yellow-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
-          >
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+        >
           Comparaison de fraction
         </Link>
       </div>
