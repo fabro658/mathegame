15c3db425a81a540c72ab8782b4558a18509564f
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-07 14:02:59 -0500

    new

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
               Primaire
             </a>
           </Link>
-          <Link href="/scolaire">
+          <Link href="/scolaire/niveaux">
             <a className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
               Secondaire
             </a>
