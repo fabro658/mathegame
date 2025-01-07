8996e73b52d803455171ad6ec68d2d948fa6a587
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-07 14:41:04 -0500

    new

--- src/app/primaire/page.tsx
+++ src/app/primaire/page.tsx
@@ -5,7 +5,13 @@ export default function Primaire() {
     <div className="min-h-screen flex flex-col justify-center items-center bg-blue-100 text-black-900">
       {/* Liens vers les niveaux */}
       <div className="flex flex-col gap-4 absolute right-8 top-1/2 transform -translate-y-1/2">
-        <Link href="/primaire/niveaux/niveau1">
+      <Link
+        href="/niveaux/niveau1"
+        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
+      >
+        Retour
+      </Link>
+      <Link href="/primaire/niveaux/niveau1">
           <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
             Niveau 1
           </div>
@@ -30,12 +36,6 @@ export default function Primaire() {
             Niveau 5
           </div>
         </Link>
-        <Link
-        href="/niveaux/niveau1"
-        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
-      >
-        Retour
-      </Link>
       </div>
     </div>
   );
