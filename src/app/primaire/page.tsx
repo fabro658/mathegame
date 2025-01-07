1638d13d42e4bcfd8ebc5f53fa5bc9e637920980
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-07 14:48:02 -0500

    Ajustement position bouton retour

--- src/app/primaire/page.tsx
+++ src/app/primaire/page.tsx
@@ -2,16 +2,18 @@ import Link from "next/link";
 
 export default function Primaire() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-blue-100 text-black-900">
-      {/* Liens vers les niveaux */}
-      <div className="flex flex-col gap-4 absolute right-8 top-1/2 transform -translate-y-1/2">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-blue-100 text-black-900 relative">
+      {/* Bouton Retour */}
       <Link
-        href="/niveaux/niveau1"
-        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
+        href="/"
+        className="absolute top-4 left-4 bg-orange-500 text-white py-3 px-8 rounded font-bold z-10"
       >
         Retour
       </Link>
-      <Link href="/primaire/niveaux/niveau1">
+
+      {/* Liens vers les niveaux */}
+      <div className="flex flex-col gap-4 absolute right-8 top-1/2 transform -translate-y-1/2 z-0">
+        <Link href="/primaire/niveaux/niveau1">
           <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
             Niveau 1
           </div>
