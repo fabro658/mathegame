c0cc541c4c73bbe1c6039763e6e830801ddbbf3b
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-08 17:14:41 -0500

    acceuil

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,65 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function Home() {
   return (
     <div
-      className="relative min-h-screen flex flex-col items-center justify-center"
+      className="relative min-h-screen"
       style={{
-        backgroundImage: "url('/fond_mathegame.png')", // Assurez-vous que l'image est dans /public
-        backgroundSize: "contain", // Garde les proportions de l'image
+        backgroundImage: "url('/fond_mathegame.png')", // Votre image ici
+        backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPosition: "center",
-        backgroundColor: "#1E2A52", // Couleur de fond pour combler si besoin
       }}
     >
-      {/* Ajout des boutons par-dessus */}
-      <div className="absolute bottom-20 flex flex-col items-center gap-6">
+      {/* Menu en haut à gauche */}
+      <header className="absolute top-4 left-4">
+        <div className="menu flex flex-col gap-4">
+          <span className="menu-title font-bold text-lg mb-4 text-white">Menu</span>
+          <Link href="/menu/apprendre" className="flex items-center gap-2 text-white hover:underline">
+            <Image src="/file.svg" alt="Apprendre Icon" width={20} height={20} />
+            <span>Apprendre</span>
+          </Link>
+          <Link href="/menu/options" className="flex items-center gap-2 text-white hover:underline">
+            <Image src="/globe.svg" alt="À propos Icon" width={20} height={20} />
+            <span>À propos</span>
+          </Link>
+        </div>
+      </header>
+
+      {/* Boutons positionnés sur les demi-cercles */}
+      <div className="relative w-full h-full">
+        {/* Bouton Primaire */}
         <Link href="/primaire">
-          <button className="bg-yellow-400 text-black font-bold py-3 px-6 rounded-lg hover:bg-yellow-500">
+          <button
+            className="absolute bg-yellow-400 text-black font-bold py-2 px-4 rounded-lg hover:bg-yellow-500"
+            style={{
+              bottom: '15%', // Ajustez la position verticale
+              left: '20%', // Ajustez la position horizontale
+              transform: 'translate(-50%, 50%)', // Centre le bouton sur le demi-cercle
+            }}
+          >
             Primaire
           </button>
         </Link>
+
+        {/* Bouton Secondaire */}
         <Link href="/secondaire">
-          <button className="bg-green-500 text-white font-bold py-3 px-6 rounded-lg hover:bg-green-600">
+          <button
+            className="absolute bg-green-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-green-600"
+            style={{
+              bottom: '15%', // Ajustez la position verticale
+              right: '20%', // Ajustez la position horizontale
+              transform: 'translate(50%, 50%)', // Centre le bouton sur le demi-cercle
+            }}
+          >
             Secondaire
           </button>
         </Link>
+      </div>
+
+      {/* Bouton Avancé en bas au centre */}
+      <div className="absolute bottom-10 w-full flex justify-center">
         <Link href="/avancé">
           <button className="bg-orange-500 text-white font-bold py-3 px-6 rounded-lg hover:bg-orange-600">
             Avancé
