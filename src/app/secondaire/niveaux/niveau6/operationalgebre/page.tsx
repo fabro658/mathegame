50561352a49fa54faeb2afcceeaec9a7c9ac518f
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-08 10:30:46 -0500

    secondaire

---
+++ src/app/secondaire/niveaux/niveau6/operationalgebre/page.tsx
@@ -0,0 +1,8 @@
+export default function Operationalgebrique() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
+      <h1 className="text-3xl font-bold mb-4">Opération Algébrique</h1>
+      <p>Bienvenue sur la page des opération alébrique !</p>
+    </div>
+  );
+}
--- src/app/secondaire/page.tsx
+++ src/app/secondaire/page.tsx
@@ -14,27 +14,37 @@ export default function Secondaire() {
     <div className="flex flex-col gap-4 absolute right-8 top-1/2 transform -translate-y-1/2 z-0">
       <Link href="/secondaire/niveaux/niveau1">
           <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
-            Niveau 1
+            Arithmétique
           </div>
         </Link>
         <Link href="/secondaire/niveaux/niveau2">
           <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
-            Niveau 2
+            Algèbre
           </div>
         </Link>
         <Link href="/secondaire/niveaux/niveau3">
           <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
-            Niveau 3
+            Probabilités
           </div>
         </Link>
         <Link href="/secondaire/niveaux/niveau4">
           <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
-            Niveau 4
+            Statistique
           </div>
         </Link>
         <Link href="/secondaire/niveaux/niveau5">
           <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
-            Niveau 5
+            Géométrie
+          </div>
+        </Link>
+        <Link href="/secondaire/niveaux/niveau6">
+          <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
+            Géométrie analytique
+          </div>
+        </Link>
+        <Link href="/secondaire/niveaux/niveau7">
+          <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
+            MAthématiques discètes
           </div>
         </Link>
       </div>
