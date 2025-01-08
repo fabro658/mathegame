6d109cf086b5c11bfde9276b9d388c47c6aacb16
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-08 09:26:38 -0500

    Bouton retour secondaire

--- src/app/secondaire/page.tsx
+++ src/app/secondaire/page.tsx
@@ -2,15 +2,16 @@ import Link from "next/link";
 
 export default function Secondaire() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-green-100 text-black-900">
-      {/* Liens vers les niveaux */}
-      <div className="flex flex-col gap-4 absolute right-8 top-1/2 transform -translate-y-1/2">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-blue-100 text-black-900 relative">
+      {/* Bouton Retour */}
       <Link
         href="/"
-        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
+        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold z-10"
       >
         Retour
       </Link>
+  {/* Liens vers les niveaux */}
+    <div className="flex flex-col gap-4 absolute right-8 top-1/2 transform -translate-y-1/2 z-0">
       <Link href="/secondaire/niveaux/niveau1">
           <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
             Niveau 1
