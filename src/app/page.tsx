056f47161a8e5194e0e0673da11666afd04c640c
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-08 18:46:19 -0500

    en

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,7 +38,7 @@ export default function Home() {
       </div>
 
       {/* Boutons Primaire et Secondaire */}
-      <div className="absolute bottom-[90px] w-full flex justify-between">
+      <div className="absolute bottom-[120px] w-full flex justify-between">
         {/* Bouton Primaire */}
         <Link href="/primaire">
           <div
