ba9ecc06a367256e63f5ec45e40368b22efdf099
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-10 21:23:53 -0500

    new

--- src/app/primaire/niveaux/niveau2/page.tsx
+++ src/app/primaire/niveaux/niveau2/page.tsx
@@ -13,10 +13,10 @@ export default function Page() {
         Retour
       </Link>
 
-      <h1 className="text-4xl font-bold mb-12">Choisissez une opération</h1>
+      <h1 className="text-4xl font-bold mb-6 sm:mb-12 text-center">Choisissez une opération</h1>
       <div className="flex flex-col sm:flex-row sm:gap-8 sm:w-full sm:justify-center">
         {/* Première ligne avec 2 boutons */}
-        <div className="flex flex-col sm:flex-row gap-8 sm:gap-8 sm:w-full sm:justify-center">
+        <div className="flex flex-col sm:flex-row gap-4 sm:gap-8 sm:w-full sm:justify-center">
           <Link
             href="/primaire/niveaux/niveau2/comparaison"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[320px] h-[120px]"
