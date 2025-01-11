edaae5873374803c6ec23bb8a911183c773ba27e
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-10 21:37:33 -0500

    new

--- src/app/primaire/niveaux/niveau2/page.tsx
+++ src/app/primaire/niveaux/niveau2/page.tsx
@@ -16,20 +16,19 @@ export default function Page() {
       <h1 className="text-4xl font-bold mb-6 sm:mb-12 text-center">Choisissez une opération</h1>
 
       {/* Disposition des boutons */}
-      <div className="flex flex-col sm:flex-row sm:flex-wrap sm:gap-8 sm:w-full sm:justify-center">
-        
+      <div className="flex flex-col gap-6 sm:gap-8 sm:w-full sm:items-center">
         {/* Première ligne avec 2 boutons */}
-        <div className="flex sm:w-full sm:justify-between gap-0 sm:gap-8">
+        <div className="flex flex-col sm:flex-row sm:w-full sm:justify-between gap-6 sm:gap-8">
           <Link
             href="/primaire/niveaux/niveau2/comparaison"
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[320px] h-[120px]"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-full sm:w-[320px] h-[120px]"
             aria-label="Page de comparaison"
           >
             Comparaisons
           </Link>
           <Link
             href="/primaire/niveaux/niveau2/expression_equivalente"
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[320px] h-[120px]"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-full sm:w-[320px] h-[120px]"
             aria-label="Page d'expression équivalente"
           >
             Expression équivalente
@@ -37,10 +36,11 @@ export default function Page() {
         </div>
 
         {/* Deuxième ligne avec 1 bouton centré */}
-        <div className="flex sm:w-full sm:justify-center sm:items-center gap-0 sm:gap-8">
+        <div className="flex sm:w-full sm:justify-center">
           <Link
             href="/primaire/niveaux/niveau2/comparer_fraction"
-            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[320px] h-[120px]"
+            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-full sm:w-[320px] h-[120px]"
+            aria-label="Page de comparaison de fraction"
           >
             Comparaison de fraction
           </Link>
