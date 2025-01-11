3bb6cc7f6899e8a624714ebe335e57989de22706
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-11 17:27:24 -0500

    new

--- src/app/primaire/niveaux/niveau2/page.tsx
+++ src/app/primaire/niveaux/niveau2/page.tsx
@@ -13,31 +13,32 @@ export default function Page() {
         Retour
       </Link>
 
-      <h1 className="text-4xl font-bold mb-12 text-center">Choisissez une opération</h1>
+      {/* Titre centré, en haut de la page */}
+      <h1 className="text-4xl font-bold mb-12 text-center mt-16 sm:mt-0">Choisissez une opération</h1>
       
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8">
         {/* Boutons en une colonne sur mobile, deux colonnes sur écrans plus grands */}
         <a
           href="/primaire/niveaux/niveau2/comparaison_decimaux"
-          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[90%] sm:w-full md:w-full"
+          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[70%] sm:w-[80%] md:w-full"
         >
           Comparaisons nombre décimaux
         </a>
         <a
           href="/primaire/niveaux/niveau2/comparaison"
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[90%] sm:w-full md:w-full"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[70%] sm:w-[80%] md:w-full"
         >
           Comparaisons
         </a>
         <a
           href="/primaire/niveaux/niveau2/expression_equivalente"
-          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[90%] sm:w-full md:w-full"
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[70%] sm:w-[80%] md:w-full"
         >
           Expression équivalente
         </a>
         <a
           href="/primaire/niveaux/niveau2/comparer_fraction"
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[90%] sm:w-full md:w-full"
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[70%] sm:w-[80%] md:w-full"
         >
           Comparaison de fraction
         </a>
