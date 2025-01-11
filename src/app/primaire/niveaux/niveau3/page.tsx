a781f0bc5d347af949043e4246e3234bea99f29a
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-11 08:49:41 -0500

    new

--- src/app/primaire/niveaux/niveau3/page.tsx
+++ src/app/primaire/niveaux/niveau3/page.tsx
@@ -15,35 +15,35 @@ export default function Page() {
 
       <h1 className="text-4xl font-bold mb-12 text-center">Choisissez une opération</h1>
       
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-        {/* Boutons en une colonne sur mobile, deux colonnes sur écrans plus grands */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-8 w-full px-4 sm:px-8 lg:px-32">
+        {/* Boutons alignés en colonne sur mobile et en trois colonnes sur écrans plus grands */}
         <a
           href="/primaire/niveaux/niveau3/additionfraction"
-          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px] sm:w-auto"
+          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
         >
           Addition de fraction
         </a>
         <a
           href="/primaire/niveaux/niveau3/soustractionfraction"
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px] sm:w-auto"
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
         >
           Soustraction de fraction
         </a>
         <a
           href="/primaire/niveaux/niveau3/multiplicationfraction"
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px] sm:w-auto"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
         >
           Multiplication de fraction
         </a>
         <a
           href="/primaire/niveaux/niveau3/divisionfraction"
-          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px] sm:w-auto"
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
         >
           Division de fraction
         </a>
         <a
           href="/primaire/niveaux/niveau3/fractionreduite"
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px] sm:w-auto"
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center"
         >
           Fraction réduite
         </a>
@@ -51,6 +51,3 @@ export default function Page() {
     </div>
   );
 }
-
-
-
