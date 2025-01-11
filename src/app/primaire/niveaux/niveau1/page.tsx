7eda3bcf0bac48d3d3a9b06a49f3b25f46e09835
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-11 09:02:42 -0500

    new

--- src/app/primaire/niveaux/niveau1/page.tsx
+++ src/app/primaire/niveaux/niveau1/page.tsx
@@ -8,36 +8,37 @@ export default function Page() {
       {/* Bouton de retour en haut à droite */}
       <Link
         href="/primaire"
-        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
+        className="absolute top-4 right-4 bg-orange-500 text-white py-2 px-6 rounded font-bold shadow"
       >
         Retour
       </Link>
 
+      {/* Titre de la page */}
       <h1 className="text-4xl font-bold mb-12 text-center">Choisissez une opération</h1>
       
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8">
-        {/* Boutons en une colonne sur mobile, deux colonnes sur écrans plus grands */}
-        <a
+      {/* Grille des boutons */}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-full px-4 sm:px-8 lg:px-32">
+      <a
           href="/primaire/niveaux/niveau1/addition"
-          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-8 rounded-lg text-2xl shadow-md text-center flex items-center justify-center"
         >
           Addition
         </a>
         <a
           href="/primaire/niveaux/niveau1/soustraction"
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-8 rounded-lg text-2xl shadow-md text-center flex items-center justify-center"
         >
           Soustraction
         </a>
         <a
           href="/primaire/niveaux/niveau1/multiplication"
-          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-4 px-8 rounded-lg text-2xl shadow-md text-center flex items-center justify-center"
         >
           Multiplication
         </a>
         <a
           href="/primaire/niveaux/niveau1/division"
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+          className="bg-red-500 hover:bg-red-700 text-white font-bold py-4 px-8 rounded-lg text-2xl shadow-md text-center flex items-center justify-center"
         >
           Division
         </a>
