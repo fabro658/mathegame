21d2000d8cf2376237fb66f6491225545a02ff98
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-11 08:14:36 -0500

    N2 et 3

--- src/app/primaire/niveaux/niveau2/page.tsx
+++ src/app/primaire/niveaux/niveau2/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900 relative">
+    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900 relative py-16">
       {/* Bouton de retour en haut à droite */}
       <Link
         href="/primaire"
@@ -15,23 +15,23 @@ export default function Page() {
 
       <h1 className="text-4xl font-bold mb-12 text-center">Choisissez une opération</h1>
       
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8">
-        {/* Boutons en une colonne sur mobile, deux colonnes sur écrans plus grands */}
+      {/* Disposition des boutons */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 w-full max-w-4xl">
         <a
-            href="/primaire/niveaux/niveau2/comparaison"
-            className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+          href="/primaire/niveaux/niveau2/comparaison"
+          className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-full"
         >
           Comparaisons
         </a>
         <a
-            href="/primaire/niveaux/niveau2/expression_equivalente"
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+          href="/primaire/niveaux/niveau2/expression_equivalente"
+          className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-full"
         >
           Expression équivalente
         </a>
         <a
-            href="/primaire/niveaux/niveau2/comparer_fraction"
-            className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
+          href="/primaire/niveaux/niveau2/comparer_fraction"
+          className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-full"
         >
           Comparaison de fraction
         </a>
@@ -39,4 +39,3 @@ export default function Page() {
     </div>
   );
 }
-
