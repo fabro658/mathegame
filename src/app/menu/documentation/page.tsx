59ee98c9dd03126825b7a7074b5782938eb28a60
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-11 18:51:05 -0500

    enw

--- src/app/menu/documentation/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Documentation() {
-  return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
-      <h1 className="text-3xl font-bold mb-4">Documentation</h1>
-      <p>Bienvenue sur la page de documentation !</p>
-    </div>
-  );
-}
