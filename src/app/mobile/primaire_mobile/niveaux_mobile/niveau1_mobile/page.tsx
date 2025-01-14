a3be00718710a12023ceea7a9ad7c5c524c1257a
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-14 16:53:16 -0500

    newq

--- src/app/mobile/primaire_mobile/niveaux_mobile/niveau1_mobile/page.tsx
+++ src/app/mobile/primaire_mobile/niveaux_mobile/niveau1_mobile/page.tsx
@@ -18,25 +18,25 @@ export default function Page() {
       {/* Boutons */}
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8 relative z-10">
         <a
-          href="/mobile/primaire-mobile/niveaux_mobile/niveau1_mobile/addition_mobile"
+          href="/mobile/primaire_mobile/niveaux_mobile/niveau1_mobile/addition_mobile"
           className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
         >
           Addition
         </a>
         <a
-          href="/mobile/primaire/niveaux_mobile/niveau1_mobile/soustraction_mobile"
+          href="/mobile/primaire_mobile/niveaux_mobile/niveau1_mobile/soustraction_mobile"
           className="bg-green-500 hover:bg-green-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
         >
           Soustraction
         </a>
         <a
-          href="/mobile/primaire/niveaux_mobile/niveau1_mobile/multiplication_mobile"
+          href="/mobile/primaire_mobile/niveaux_mobile/niveau1_mobile/multiplication_mobile"
           className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
         >
           Multiplication
         </a>
         <a
-          href="/mobile/primaire/niveaux_mobile/niveau1_mobile/division_mobile"
+          href="/mobile/primaire_mobile/niveaux_mobile/niveau1_mobile/division_mobile"
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
         >
           Division
