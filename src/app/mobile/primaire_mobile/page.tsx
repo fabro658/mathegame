f19d8e77b47d44a8837dfdb0412f1b454639b2dc
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-14 13:10:02 -0500

    Essai mobile

--- src/app/mobile/primaire_mobile/page.tsx
+++ src/app/mobile/primaire_mobile/page.tsx
@@ -24,32 +24,32 @@ export default function Primaire_mobile() {
 
       {/* Liens vers les niveaux */}
       <div className="flex flex-col gap-4 w-full px-4 sm:items-center sm:gap-4 sm:w-full sm:text-center sm:mt-8 md:absolute md:right-0 md:top-1/2 md:transform md:-translate-y-1/2 md:flex-col md:items-end md:w-auto z-0">
-        <Link href="/mobile/primaire_mobile/niveaux_mobile/niveau1_mobile">
+        <Link href="/primaire_mobile/niveaux_mobile/niveau1_mobile">
           <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-yellow-500 text-white gap-2 hover:bg-blue-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72 md:mr-[-36px]">
             Opérations arithmétiques
           </div>
         </Link>
-        <Link href="/mobile/primaire_mobile/niveaux_mobile//niveau2_mobile">
+        <Link href="/primaire_mobile/niveaux_mobile//niveau2_mobile">
           <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-orange-500 text-white gap-2 hover:bg-green-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72 md:mr-[-36px]">
             Comparaisons
           </div>
         </Link>
-        <Link href="/mobile/primaire_mobile/niveaux_mobile//niveau3_mobile">
+        <Link href="/primaire_mobile/niveaux_mobile//niveau3_mobile">
           <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-red-500 text-white gap-2 hover:bg-red-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72 md:mr-[-36px]">
             Fraction
           </div>
         </Link>
-        <Link href="/mobile/primaire_mobile/niveaux_mobile//niveau4_mobile">
+        <Link href="/primaire_mobile/niveaux_mobile//niveau4_mobile">
           <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-blue-800 text-white gap-2 hover:bg-blue-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72 md:mr-[-36px]">
             Géométrie
           </div>
         </Link>
-        <Link href="/mobile/primaire_mobile/niveaux_mobile//niveau5_mobile">
+        <Link href="/primaire_mobile/niveaux_mobile//niveau5_mobile">
           <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-blue-500 text-white gap-2 hover:bg-green-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72 md:mr-[-36px]">
             Exposants
           </div>
         </Link>
-        <Link href="/mobile/primaire_mobile/niveaux_mobile//niveau6_mobile">
+        <Link href="/primaire_mobile/niveaux_mobile//niveau6_mobile">
           <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-purple-500 text-white gap-2 hover:bg-red-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72 md:mr-[-36px]">
             Priorité d&#39;opération
           </div>
--- src/app/primaire/niveaux/niveau3/page.tsx
+++ src/app/primaire/niveaux/niveau3/page.tsx
@@ -7,22 +7,22 @@ export default function Page() {
     <div className="min-h-screen relative bg-gray-100 text-gray-900 flex flex-col items-center justify-center">
       {/* Lignes verticales */}
       <div className="absolute top-0 left-0 h-full w-full flex">
-        <div className="w-[5%] bg-blue-900"></div>
-        <div className="w-[5%] bg-blue-700"></div>
-        <div className="w-[5%] bg-blue-500"></div>
-        <div className="w-[5%] bg-blue-300"></div>
-        <div className="w-[5%] bg-blue-100"></div>
+        <div className="w-[3%] bg-blue-900"></div>
+        <div className="w-[3%] bg-blue-700"></div>
+        <div className="w-[3%] bg-blue-500"></div>
+        <div className="w-[3%] bg-blue-300"></div>
+        <div className="w-[3%] bg-blue-100"></div>
       </div>
 
       {/* Contenu principal */}
       <div className="relative z-10 text-center">
         {/* Bouton de retour */}
-        <Link
-          href="/primaire"
-          className="absolute top-4 right-4 bg-orange-500 text-white py-2 px-6 rounded font-bold shadow"
-        >
-          Retour
-        </Link>
+      <Link
+        href="/primaire"
+        className="absolute top-4 right-4 bg-orange-500 text-white py-2 px-6 rounded font-bold shadow"
+      >
+        Retour
+      </Link>
 
         {/* Titre */}
         <h1 className="text-3xl font-bold mb-12">Choisissez une opération</h1>
