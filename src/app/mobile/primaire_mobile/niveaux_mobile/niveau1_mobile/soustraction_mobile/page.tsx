a93cca908134197af3ed3875ac4151a698026cdb
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-16 08:35:51 -0500

    new

--- src/app/mobile/primaire_mobile/niveaux_mobile/niveau1_mobile/soustraction_mobile/page.tsx
+++ src/app/mobile/primaire_mobile/niveaux_mobile/niveau1_mobile/soustraction_mobile/page.tsx
@@ -68,14 +68,16 @@ export default function Soustraction() {
     <div className="flex flex-col items-center justify-between min-h-screen bg-gray-100 text-black py-6 px-4">
       {/* Conteneur pour les boutons */}
       <div className="flex justify-between w-full mb-6">
-        <Link href="/mobile/menu_mobile/apprendre_mobile/opérations arithmétiques_mobile" className="bg-black text-white py-3 px-8 rounded font-bold">
-          Apprendre
-        </Link>
-        <Link 
-        href="/mobile/primaire_mobile/niveaux_mobile">
-        className="bg-orange-500 text-white py-3 px-8 rounded font-bold"
-          Retour
-        </Link>
+      <Link 
+  href="/mobile/menu_mobile/apprendre_mobile/opérations arithmétiques_mobile" 
+  className="bg-black text-white py-3 px-8 rounded font-bold">
+  Apprendre
+</Link>
+<Link 
+  href="/mobile/primaire_mobile/niveaux_mobile"
+  className="bg-orange-500 text-white py-3 px-8 rounded font-bold">
+  Retour
+</Link>
       </div>
 
       {/* Titre */}
