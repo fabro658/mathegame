afaef3ed7be3899eafe43c2321bde9e5ac44c814
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-16 08:27:55 -0500

    N2 q

--- src/app/primaire/niveaux/niveau2/page.tsx
+++ src/app/primaire/niveaux/niveau2/page.tsx
@@ -26,28 +26,28 @@ export default function Page() {
       {/* Boutons */}
       <div className="grid grid-cols-2 gap-4 w-full max-w-4xl px-4 sm:px-8 justify-center items-center">
         <a
-          href="/primaire/niveaux/niveau3/additionfraction"
+          href="/primaire/niveaux/niveau3/comparaison"
           className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-4 px-8 rounded-lg text-2xl shadow-md text-center flex items-center justify-center"
         >
-          Addition de fraction
+          Comparaison
         </a>
         <a
-          href="/primaire/niveaux/niveau3/soustractionfraction"
+          href="/primaire/niveaux/niveau3/comparaison_decimaux"
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-4 px-8 rounded-lg text-2xl shadow-md text-center flex items-center justify-center"
         >
-          Soustraction de fraction
+          Comparaison de nombre décimaux
         </a>
         <a
-          href="/primaire/niveaux/niveau3/multiplicationfraction"
+          href="/primaire/niveaux/niveau3/comparer_fraction"
           className="bg-green-500 hover:bg-green-700 text-white font-bold py-4 px-8 rounded-lg text-2xl shadow-md text-center flex items-center justify-center"
         >
-          Multiplication de fraction
+         COmparaison de fraction
         </a>
         <a
-          href="/primaire/niveaux/niveau3/divisionfraction"
+          href="/primaire/niveaux/niveau3/expression_equivalente"
           className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-4 px-8 rounded-lg text-2xl shadow-md text-center flex items-center justify-center"
         >
-          Division de fraction
+          Expression équivalente
         </a>
       </div>
     </div>
