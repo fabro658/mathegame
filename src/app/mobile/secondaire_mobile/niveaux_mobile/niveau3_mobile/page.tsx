eaa3aeccbc8878a300313b4dddd69ccc964bdbb4
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-01-30 18:34:40 -0500

    new

--- src/app/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile/page.tsx
+++ src/app/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile/page.tsx
@@ -32,12 +32,12 @@ export default function Niveau1() {
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau1_mobile/expression_equivalente_mobile">
           <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
-            Expression équivalent
+            Expression équivalente
           </div>
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau1_mobile/priooperation_mobile">
           <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
-            Priorité d'opération
+            Priorité d&apos;opération
           </div>
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau1_mobile/comparaison_mobile">
