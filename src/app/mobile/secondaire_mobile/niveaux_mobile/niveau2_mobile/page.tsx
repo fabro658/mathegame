8c193b4d770d4e3708c26478d46f6cc87c931085
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-02-01 15:32:06 -0500

    new

--- src/app/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/page.tsx
+++ src/app/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/page.tsx
@@ -45,11 +45,6 @@ export default function Niveau2() {
         Transformation
           </div>
         </Link>
-        <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/approx_mobile">
-        <div className="w-80 sm:w-96 rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl h-14 sm:h-16 px-8 sm:px-10 font-semibold">
-        Approximation
-          </div>
-        </Link>
       </div>
     </div>
   );
