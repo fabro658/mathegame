88606358022c2d4f55272edd4d28367b23c64ec2
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-02-01 12:07:28 -0500

    new

--- src/app/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/page.tsx
+++ src/app/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/page.tsx
@@ -13,26 +13,41 @@ export default function Niveau2() {
   
       {/* Section Titre */}
       <div className="flex flex-col items-center text-center mb-8">
-        <h1 className="text-black text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold mb-4">Exposants</h1>
+        <h1 className="text-black text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold mb-4">Fraction</h1>
         <div className="w-48 md:w-64 lg:w-96 h-1 bg-black mb-4"></div>
         <h2 className="text-black text-xl sm:text-2xl md:text-4xl lg:text-5xl font-semibold">Choisi un sujet!</h2>
       </div>
       
       {/* Boutons centrés */}
       <div className="flex flex-col gap-6 w-full max-w-md">
-        <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile/expo_mobile/n1_mobile">
-        <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-purple-500 text-white gap-2 hover:bg-red-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72 md:mr-[-36px]">
+        <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/additionfraction_mobile">
+        <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
         Addition de fraction
           </div>
         </Link>
-        <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile/expo_mobile/n2_mobile">
-        <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-purple-500 text-white gap-2 hover:bg-red-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72 md:mr-[-36px]">
+        <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/soustractionfraction_mobile">
+        <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
         Soustraction de fraction
           </div>
         </Link>
-        <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile/expo_mobile/n3_mobile">
-        <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-purple-500 text-white gap-2 hover:bg-red-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72 md:mr-[-36px]">
+        <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/multiplicationfraction_mobile">
+        <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
         Multiplication de fraction
+          </div>
+        </Link>
+        <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/divisionfraction_mobile">
+        <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
+        Division de fraction
+          </div>
+        </Link>
+        <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/trans_pourcent_mobile">
+        <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
+        Transformation
+          </div>
+        </Link>
+        <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau2_mobile/approx_mobile">
+        <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
+        Approximation
           </div>
         </Link>
       </div>
--- src/app/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile/expo_mobile/page.tsx
+++ src/app/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile/expo_mobile/page.tsx
@@ -15,24 +15,24 @@ export default function Niveau2() {
       <div className="flex flex-col items-center text-center mb-8">
         <h1 className="text-black text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold mb-4">Exposants</h1>
         <div className="w-48 md:w-64 lg:w-96 h-1 bg-black mb-4"></div>
-        <h2 className="text-black text-xl sm:text-2xl md:text-4xl lg:text-5xl font-semibold">Choisi un sujet!</h2>
+        <h2 className="text-black text-xl sm:text-2xl md:text-4xl lg:text-5xl font-semibold">Choisi un niveau!</h2>
       </div>
       
       {/* Boutons centrés */}
-      <div className="flex flex-col gap-6 items-center w-full max-w-md">
+      <div className="flex flex-col gap-8 items-center w-full max-w-md">
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile/expo_mobile/n1_mobile">
-          <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-purple-500 text-white gap-2 hover:bg-red-600 text-sm sm:text-base h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72">
-            Addition de fraction
+          <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-purple-500 text-white gap-2 hover:bg-red-600 text-lg sm:text-xl h-16 w-full sm:w-72 px-6 sm:px-8 md:w-96">
+            Niveau 1
           </div>
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile/expo_mobile/n2_mobile">
-          <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-purple-500 text-white gap-2 hover:bg-red-600 text-sm sm:text-base h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72">
-            Soustraction de fraction
+          <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-purple-500 text-white gap-2 hover:bg-red-600 text-lg sm:text-xl h-16 w-full sm:w-72 px-6 sm:px-8 md:w-96">
+            Niveau 2
           </div>
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile/expo_mobile/n3_mobile">
-          <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-purple-500 text-white gap-2 hover:bg-red-600 text-sm sm:text-base h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72">
-            Multiplication de fraction
+          <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-purple-500 text-white gap-2 hover:bg-red-600 text-lg sm:text-xl h-16 w-full sm:w-72 px-6 sm:px-8 md:w-96">
+            Niveau 3
           </div>
         </Link>
       </div>
