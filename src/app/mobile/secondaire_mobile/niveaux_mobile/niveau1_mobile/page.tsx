dab6a1e828ab96092901cca5250e07128334fc86
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-02-01 11:06:42 -0500

    new

--- src/app/mobile/secondaire_mobile/niveaux_mobile/niveau1_mobile/page.tsx
+++ src/app/mobile/secondaire_mobile/niveaux_mobile/niveau1_mobile/page.tsx
@@ -6,7 +6,7 @@ export default function Niveau1() {
       {/* Bouton Retour */}
       <Link
         href="/mobile/secondaire_mobile"
-        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold z-10"
+        className="absolute top-4 right-4 bg-orange-500 text-white py-4 px-10 rounded-lg font-bold text-lg sm:text-xl z-10"
       >
         Retour
       </Link>
@@ -17,31 +17,31 @@ export default function Niveau1() {
         <div className="w-48 md:w-64 lg:w-96 h-1 bg-black mb-4"></div>
         <h2 className="text-black text-xl sm:text-2xl md:text-4xl lg:text-5xl font-semibold">Choisi un sujet!</h2>
       </div>
-      
+
       {/* Liens vers les niveaux */}
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-6">
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau1_mobile/sommes_mobile">
-          <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-6 sm:px-8">
+          <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
             Sommes
           </div>
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau1_mobile/difference_mobile">
-          <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-6 sm:px-8">
+          <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
             Différence
           </div>
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau1_mobile/produit_mobile">
-          <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-6 sm:px-8">
+          <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
             Produit
           </div>
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau1_mobile/quotient_mobile">
-          <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-6 sm:px-8">
+          <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
             Quotient
           </div>
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau1_mobile/comparaison_mobile">
-          <div className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-sm sm:text-base h-10 sm:h-12 px-6 sm:px-8">
+          <div className="w-48 sm:w-64 md:w-72 h-12 sm:h-16 rounded-full border border-transparent transition-colors flex items-center justify-center bg-white text-black gap-2 hover:bg-gray-200 text-lg sm:text-xl font-bold">
             Comparer
           </div>
         </Link>
