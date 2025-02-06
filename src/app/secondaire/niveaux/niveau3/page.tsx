3de7c98cdb74605d320eb0a4dca202b972436cb5
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-02-06 18:37:37 -0500

    new

--- src/app/secondaire/niveaux/niveau3/page.tsx
+++ src/app/secondaire/niveaux/niveau3/page.tsx
@@ -1,17 +1,19 @@
-import Link from 'next/link'; // Si vous utilisez Next.js
+import Link from 'next/link';
 
 export default function Niveau3() {
   return (
     <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900">
       <h1 className="text-4xl font-bold mb-12">Niveau 3 - Choisissez une opération</h1>
-      <div className="flex flex-col items-center gap-8">
-        <div className="flex gap-8">
-        <Link
+      
+      <Link
         href="/secondaire"
         className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
       >
         Retour
       </Link>
+      
+      <div className="flex flex-col items-center gap-8">
+        <div className="flex gap-8">
           <Link
             href="/niveaux/niveau3/expo"
             className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
@@ -25,22 +27,25 @@ export default function Niveau3() {
             Racine
           </Link>
         </div>
+        
         <Link
           href="/niveaux/niveau3/expression_equivalente"
           className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
-          >
+        >
           Expression équivalente 
         </Link>
+        
         <Link
           href="/niveaux/niveau3/priooperation"
           className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
-          >
-          Priorité d'oprération
+        >
+          Priorité d&apos;opération
         </Link>
+        
         <Link
           href="/niveaux/niveau3/approx"
           className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
-          >
+        >
           Approximation
         </Link>
       </div>
