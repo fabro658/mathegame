09f028fc8edcfc463a675b2c7b9e9b4f2c06f230
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-02-24 08:25:46 -0500

    new

--- src/app/secondaire/page.tsx
+++ src/app/secondaire/page.tsx
@@ -9,8 +9,11 @@ export default function Secondaire() {
       }}
     >
       {/* Animation de la neige */}
-      <div className="snowfall"></div>
-      <h1 className="text-white text-4xl font-bold">Secondaire</h1>
+      <div className="snowfall">
+        {Array.from({ length: 50 }).map((_, i) => (
+          <div key={i} className="snowflake"></div>
+        ))}
+      </div>
 
       {/* Bouton Retour */}
       <Link
@@ -30,8 +33,9 @@ export default function Secondaire() {
           Choisis un sujet!
         </h2>
       </div>
-       {/* Liens vers les niveaux */}
-       <div className="flex flex-col gap-4 w-full px-4 sm:items-center sm:gap-4 sm:w-full sm:text-center sm:mt-8 md:absolute md:right-0 md:top-1/2 md:transform md:-translate-y-1/2 md:flex-col md:items-end md:w-auto z-0">
+
+      {/* Liens vers les niveaux */}
+      <div className="flex flex-col gap-4 w-full px-4 sm:items-center sm:gap-4 sm:w-full sm:text-center sm:mt-8 md:absolute md:right-0 md:top-1/2 md:transform md:-translate-y-1/2 md:flex-col md:items-end md:w-auto z-0">
         <Link href="/secondaire/niveaux/niveau1">
           <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-yellow-500 text-white gap-2 hover:bg-blue-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72">
             Arithmétique
@@ -58,6 +62,6 @@ export default function Secondaire() {
           </div>
         </Link>
       </div>
-      </div>
+    </div>
   );
 }
