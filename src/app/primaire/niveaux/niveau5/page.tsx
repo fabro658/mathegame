146c8e1e3438e438132bb57dd312dd1bb5d27161
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-03-06 17:12:48 -0500

    new

--- src/app/primaire/niveaux/niveau5/page.tsx
+++ src/app/primaire/niveaux/niveau5/page.tsx
@@ -1,25 +1,24 @@
-"use client";
-
 import Link from "next/link";
 
-export default function Page() {
+export default function Niveau1() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900 relative">
-      
-      {/* Bouton de retour en haut à droite */}
+    <div className="min-h-screen flex flex-col justify-center items-center bg-blue-100 text-black-900 relative">
+      {/* Bouton Retour */}
       <Link
-        href="/primaire"
-        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
+        href="/primaire/"
+        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold z-10"
       >
         Retour
       </Link>
 
-      {/* Titre */}
-      <h1 className="text-4xl font-bold mb-12 text-center">Choisissez une opération</h1>
-
+      {/* Section Titre */}
+      <div className="flex flex-col items-center text-center mb-8">
+        <h1 className="text-black text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold mb-4">Exposant!</h1>
+        <div className="w-48 md:w-64 lg:w-96 h-1 bg-black mb-4"></div>
+        <h2 className="text-black text-xl sm:text-2xl md:text-4xl lg:text-5xl font-semibold">Choisi un sujet!</h2>
+      </div>
       {/* Boutons */}
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
-        {/* Boutons en une colonne sur mobile, deux colonnes sur écrans plus grands */}
         <a
           href="/primaire/niveaux/niveau5/exposant/n1"
           className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center min-w-[300px]"
--- src/app/primaire/niveaux/niveau6/page.tsx
+++ src/app/primaire/niveaux/niveau6/page.tsx
@@ -1,19 +1,22 @@
-"use client";
-
 import Link from "next/link";
 
-export default function Page() {
+export default function Niveau1() {
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center bg-gray-100 text-gray-900 relative">
-      {/* Bouton de retour en haut à droite */}
+    <div className="min-h-screen flex flex-col justify-center items-center bg-blue-100 text-black-900 relative">
+      {/* Bouton Retour */}
       <Link
-        href="/primaire"
-        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
+        href="/primaire/"
+        className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold z-10"
       >
         Retour
       </Link>
 
-      <h1 className="text-4xl font-bold mb-12 text-center">Choisissez une opération</h1>
+      {/* Section Titre */}
+      <div className="flex flex-col items-center text-center mb-8">
+        <h1 className="text-black text-3xl sm:text-4xl md:text-6xl lg:text-7xl font-bold mb-4">Priorité d'opération</h1>
+        <div className="w-48 md:w-64 lg:w-96 h-1 bg-black mb-4"></div>
+        <h2 className="text-black text-xl sm:text-2xl md:text-4xl lg:text-5xl font-semibold">Choisi un sujet!</h2>
+      </div>
 
       <div className="flex flex-col items-center justify-center gap-8">
         {/* Le bouton centré */}
