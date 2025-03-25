acf0c26a98e83cff8ea380af18d79fc4b0859fbc
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-03-25 12:59:27 -0400

    new

--- src/app/primaire/niveaux/niveau5/exposant/n2/page.tsx
+++ src/app/primaire/niveaux/niveau5/exposant/n2/page.tsx
@@ -107,10 +107,12 @@ export default function ExponentsPractice() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-black relative">
-      <Link href="/menu/apprendre" className="absolute bottom-4 left-4 bg-black text-white py-3 px-8 rounded font-bold">
+      <Link href="/menu/apprendre" 
+      className="absolute bottom-4 left-4 bg-black text-white py-3 px-8 rounded font-bold">
         Apprendre
       </Link>
-      <Link href="/primaire/niveaux/niveau5" className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold">
+      <Link href="/primaire/niveaux/niveau5" 
+      className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold">
         Retour
       </Link>
 
