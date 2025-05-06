95af916c61a4d44947b54f54a37ed64dff18e0f1
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-05-06 17:46:31 -0400

    new

--- src/app/Hiver/page.tsx
+++ src/app/Hiver/page.tsx
@@ -3,7 +3,6 @@
 import { useEffect } from "react";
 import Link from "next/link";
 
-
 export default function Primaire() {
   useEffect(() => {
     const snowContainer = document.querySelector(".snowfall");
@@ -13,30 +12,27 @@ export default function Primaire() {
       for (let i = 0; i < 100; i++) {
         const snowflake = document.createElement("li");
         snowflake.style.left = `${Math.random() * 100}%`;
-        snowflake.style.top = `-10px`; // Start above the viewport
+        snowflake.style.top = `-10px`;
         const size = Math.random() * 10 + 5;
         snowflake.style.width = `${size}px`;
         snowflake.style.height = `${size}px`;
         snowflake.style.animationDuration = `${Math.random() * 10 + 5}s`;
-        snowflake.style.animationDelay = `${Math.random() * 20}s`; // Increased delay range for more progressive start
+        snowflake.style.animationDelay = `${Math.random() * 20}s`;
         snowContainer.appendChild(snowflake);
         console.log("Snowflake added:", snowflake);
       }
     }
   }, []);
-    return (
-      <div
-        className="min-h-screen flex flex-col justify-center items-center relative overflow-hidden"
-        style={{
-          background: "linear-gradient(180deg, #142852, #0e022e)",
-        }}
 
-      </div>
-            {/* Animation de neige */}
-            <ul className="snowfall absolute inset-0 pointer-events-none"></ul>
+  return (
+    <div
+      className="min-h-screen flex flex-col justify-center items-center relative overflow-hidden"
+      style={{
+        background: "linear-gradient(180deg, #142852, #0e022e)",
+      }}
+    >
+      {/* Animation de neige */}
+      <ul className="snowfall absolute inset-0 pointer-events-none"></ul>
     </div>
   );
 }
-
-
-
