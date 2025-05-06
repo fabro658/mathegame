88dcdb2f718b56b5c526b5c09cadae674c93328e
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-05-06 18:43:00 -0400

    new

--- src/app/secondaire/page.tsx
+++ src/app/secondaire/page.tsx
@@ -42,29 +42,29 @@ export default function Primaire() {
         </h2>
       </div>
       {/* Liens vers les niveaux */}
-<div className="flex flex-col gap-4 w-full px-4 sm:items-center sm:gap-4 sm:w-full sm:text-center sm:mt-8 md:absolute md:right-0 md:top-1/2 md:transform md:-translate-y-1/2 md:flex-col md:items-end md:w-auto z-20">
+      <div className="flex flex-col gap-4 px-4 sm:items-center sm:gap-4 sm:mt-8 md:absolute md:right-0 md:top-1/2 md:transform md:-translate-y-1/2 md:flex-col md:items-end z-20">
   <Link href="/secondaire/niveaux/niveau1">
-    <div className="w-full rounded-lg bg-yellow-500 text-white hover:bg-yellow-600 text-xl font-semibold py-4 px-6 text-center shadow-lg">
+    <div className="w-72 rounded-lg bg-yellow-500 text-white hover:bg-yellow-600 text-xl font-semibold py-4 px-6 text-center shadow-lg">
       Arithmétique
     </div>
   </Link>
   <Link href="/secondaire/niveaux/niveau2">
-    <div className="w-full rounded-lg bg-orange-500 text-white hover:bg-orange-600 text-xl font-semibold py-4 px-6 text-center shadow-lg">
+    <div className="w-72 rounded-lg bg-orange-500 text-white hover:bg-orange-600 text-xl font-semibold py-4 px-6 text-center shadow-lg">
       Fractions
     </div>
   </Link>
   <Link href="/secondaire/niveaux/niveau3">
-    <div className="w-full rounded-lg bg-red-500 text-white hover:bg-red-600 text-xl font-semibold py-4 px-6 text-center shadow-lg">
+    <div className="w-72 rounded-lg bg-red-500 text-white hover:bg-red-600 text-xl font-semibold py-4 px-6 text-center shadow-lg">
       Probabilités
     </div>
   </Link>
   <Link href="/secondaire/niveaux/niveau4">
-    <div className="w-full rounded-lg bg-blue-700 text-white hover:bg-blue-800 text-xl font-semibold py-4 px-6 text-center shadow-lg">
+    <div className="w-72 rounded-lg bg-blue-700 text-white hover:bg-blue-800 text-xl font-semibold py-4 px-6 text-center shadow-lg">
       Géométrie
     </div>
   </Link>
   <Link href="/secondaire/niveaux/niveau5">
-    <div className="w-full rounded-lg bg-green-600 text-white hover:bg-green-700 text-xl font-semibold py-4 px-6 text-center shadow-lg">
+    <div className="w-72 rounded-lg bg-green-600 text-white hover:bg-green-700 text-xl font-semibold py-4 px-6 text-center shadow-lg">
       Algèbre
     </div>
   </Link>
