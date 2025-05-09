0ec971c19b30315bb0995c245cc9b4a426e0d971
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-05-09 14:07:56 -0400

    new

--- src/app/secondaire/niveaux/niveau1/difference/page.tsx
+++ src/app/secondaire/niveaux/niveau1/difference/page.tsx
@@ -98,7 +98,7 @@ export default function ComparerEntiers() {
         Apprendre
       </Link>
       <Link
-        href="/secondaire/niveaux/niveau1"
+        href="/primaire/niveaux/niveau2"
         className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold"
       >
         Retour
