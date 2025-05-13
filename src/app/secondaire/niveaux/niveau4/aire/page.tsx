fa2def8283c1fa8d4fd97ddee6fb69e9334e8bc1
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-05-13 15:30:53 -0400

    new

--- src/app/secondaire/niveaux/niveau4/aire/page.tsx
+++ src/app/secondaire/niveaux/niveau4/aire/page.tsx
@@ -141,7 +141,7 @@ export default function Perimetre() {
       <Link href="/menu/apprendre" className="absolute bottom-4 left-4 bg-black text-white py-3 px-8 rounded font-bold">
         Apprendre
       </Link>
-      <Link href="/primaire/niveaux/niveau4" className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold">
+      <Link href="/secondaire/niveaux/niveau4" className="absolute top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold">
         Retour
       </Link>
 
