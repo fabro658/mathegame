e7004ce8313069e93ed7b15af168af624b0ca14e
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-05-22 16:01:42 -0400

    new

--- src/app/primaire/niveaux/niveau2/page.tsx
+++ src/app/primaire/niveaux/niveau2/page.tsx
@@ -11,10 +11,25 @@ export default function Niveau2() {
         Retour
       </Link>
 
+      {/* Lune et étoiles */}
+      <div className="moon">
+        <div className="crat crat1"></div>
+        <div className="crat crat2"></div>
+        <div className="crat crat3"></div>
+        <div className="crat crat4"></div>
+        <div className="crat crat5"></div>
+        <div className="crat crat6"></div>
+      </div>
+
+      <div className="star star1"></div>
+      <div className="star star2"></div>
+      <div className="star star3"></div>
+      <div className="star star4"></div>
+
       {/* Titre thématique */}
       <div className="flex flex-col items-center text-center mb-12 z-10">
         <h1 className="text-black text-5xl sm:text-6xl md:text-7xl font-extrabold mb-3 drop-shadow">
-          🌠 Mission Comparaison
+          Mission Comparaison
         </h1>
         <p className="text-xl sm:text-2xl text-gray-700 font-medium">
           Explore les nombres dans l’univers mathématique 🚀
@@ -27,25 +42,25 @@ export default function Niveau2() {
           href="/primaire/niveaux/niveau2/comparaison"
           className="bg-purple-600 hover:bg-purple-800 text-white font-bold py-6 px-12 rounded-2xl text-3xl shadow-xl flex items-center justify-center min-w-[300px] transition transform hover:scale-105"
         >
-          Comparaison 🔭 
+          Comparaison
         </a>
         <a
           href="/primaire/niveaux/niveau2/comparaison_decimaux"
           className="bg-green-600 hover:bg-green-800 text-white font-bold py-6 px-12 rounded-2xl text-3xl shadow-xl flex items-center justify-center min-w-[300px] transition transform hover:scale-105"
         >
-          Décimaux 🦠 
+          Décimaux
         </a>
         <a
           href="/primaire/niveaux/niveau2/comparer_fraction"
           className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-6 px-12 rounded-2xl text-3xl shadow-xl flex items-center justify-center min-w-[300px] transition transform hover:scale-105"
         >
-          Fractions 🚁 
+          Fractions
         </a>
         <a
           href="/primaire/niveaux/niveau2/expression_equivalente"
           className="bg-red-500 hover:bg-red-700 text-white font-bold py-6 px-12 rounded-2xl text-3xl shadow-xl flex items-center justify-center min-w-[300px] transition transform hover:scale-105"
         >
-          Expressions équivalentes 🧪 
+          Expressions équivalentes
         </a>
       </div>
     </div>
