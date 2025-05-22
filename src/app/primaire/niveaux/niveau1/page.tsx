8a61c2fe139e69271fbc6047ab5d84986f26bf61
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-05-22 09:23:06 -0400

    new

--- src/app/primaire/niveaux/niveau1/page.tsx
+++ src/app/primaire/niveaux/niveau1/page.tsx
@@ -5,6 +5,13 @@ export default function Niveau1() {
     <div className="min-h-screen flex flex-col justify-center items-center text-black relative overflow-hidden bg-[#71c6f7] font-fredoka">
       {/* Décor ciel */}
       <div className="absolute inset-0 z-0">
+        {/* Nuages CSS décoratifs */}
+        <div className="absolute top-0 left-0 w-full h-48 z-0 pointer-events-none">
+          <div className="cloud absolute top-4 left-10 scale-[0.6]"></div>
+          <div className="cloud absolute top-10 left-1/3 scale-[0.8]"></div>
+          <div className="cloud absolute top-2 right-12 scale-[0.7]"></div>
+        </div>
+
         {/* Visage nuageux */}
         <div className="absolute top-10 left-10 z-10">
           <svg width="80" height="80" viewBox="0 0 100 100">
@@ -15,26 +22,7 @@ export default function Niveau1() {
             <path d="M30,45 Q35,50 40,45" stroke="white" strokeWidth="2" fill="none" />
           </svg>
         </div>
-
-        {/* Sol jaune et vague */}
-        <div className="absolute bottom-0 left-0 w-full h-40">
-          <svg width="100%" height="100%" preserveAspectRatio="none">
-            <rect width="100%" height="100%" fill="#fcd34d" />
-            <path d="M0,30 Q100,60 200,30 T400,30 T600,30 T800,30 T1000,30" fill="#38bdf8" />
-          </svg>
         </div>
-
-        {/* Arbre tropical */}
-        <div className="absolute bottom-20 left-6 z-10">
-          <svg width="100" height="120" viewBox="0 0 100 120">
-            <rect x="45" y="40" width="10" height="70" fill="#8b5e3c" />
-            <path d="M50,40 Q30,10 10,20 Q30,30 50,40" fill="#34d399" />
-            <path d="M50,40 Q70,10 90,20 Q70,30 50,40" fill="#34d399" />
-            <path d="M50,40 Q20,30 20,50 Q40,50 50,40" fill="#34d399" />
-            <path d="M50,40 Q80,30 80,50 Q60,50 50,40" fill="#34d399" />
-          </svg>
-        </div>
-      </div>
 
       {/* Bouton retour */}
       <Link
@@ -52,8 +40,8 @@ export default function Niveau1() {
         </p>
       </div>
 
-      {/* Boutons 2x2 capsules */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 z-10 mt-4">
+      {/* Boutons missions */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 z-10 mt-4 px-4">
         <a
           href="/primaire/niveaux/niveau1/addition"
           className="bg-purple-600 text-white font-bold font-fredoka text-3xl px-12 py-6 rounded-full text-center transition transform hover:scale-105"
