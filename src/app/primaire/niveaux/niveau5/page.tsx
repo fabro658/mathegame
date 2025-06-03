08c327f435e28ce80f131128d9b6c90756635adf
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-06-03 14:56:30 -0400

    new

--- src/app/primaire/niveaux/niveau5/page.tsx
+++ src/app/primaire/niveaux/niveau5/page.tsx
@@ -65,17 +65,18 @@ export default function Niveau5() {
       {/* Boutons */}
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-8 z-10">
         <a
-          href="/primaire/niveaux/niveau5/exposant/n1"
-          className="bg-purple-600 hover:bg-purple-800 text-white font-bold py-6 px-12 rounded-xl text-3xl shadow-xl text-center flex items-center justify-center min-w-[300px] transition transform hover:scale-105"
-        >
-          Premier niveau
-        </a>
-        <a
-          href="/primaire/niveaux/niveau5/exposant/n2"
-          className="bg-green-600 hover:bg-green-800 text-white font-bold py-6 px-12 rounded-xl text-3xl shadow-xl text-center flex items-center justify-center min-w-[300px] transition transform hover:scale-105"
-        >
-          Deuxième niveau
-        </a>
+  href="/primaire/niveaux/niveau5/exposant/n1"
+  className="btn-candy text-white font-bold py-6 px-12 rounded-xl text-3xl shadow-xl text-center flex items-center justify-center min-w-[300px] transition transform hover:scale-105"
+>
+  Premier niveau
+</a>
+<a
+  href="/primaire/niveaux/niveau5/exposant/n2"
+  className="btn-candy text-white font-bold py-6 px-12 rounded-xl text-3xl shadow-xl text-center flex items-center justify-center min-w-[300px] transition transform hover:scale-105"
+>
+  Deuxième niveau
+</a>
+
       </div>
     </div>
   );
