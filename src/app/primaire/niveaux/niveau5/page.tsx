75c55d2aa96da9a409290ebb6e93fc14c63ecc18
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-06-03 11:36:51 -0400

    new

--- src/app/primaire/niveaux/niveau5/page.tsx
+++ src/app/primaire/niveaux/niveau5/page.tsx
@@ -43,12 +43,6 @@ export default function Niveau5() {
   <div className="snowman-mouth-3"></div>
   <div className="snowman-mouth-4"></div>
   <div className="snowman-mouth-5"></div>
-  <div className="hat-1"></div>
-  <div className="hat-2"></div>
-  <div className="hat-3"></div>
-  <div className="scarf-1"></div>
-  <div className="scarf-2"></div>
-  <div className="scarf-3"></div>
 </div>
         {/* Étoiles */}
         <div className="star-1 stars"></div>
