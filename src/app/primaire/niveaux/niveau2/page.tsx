ebf6ecd4fbbe4161af91f4c5fe51a919bf0cedcd
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-06-12 10:28:02 -0400

    new

--- src/app/primaire/niveaux/niveau2/page.tsx
+++ src/app/primaire/niveaux/niveau2/page.tsx
@@ -9,6 +9,7 @@ export default function Niveau2() {
     <div className="flamme"></div>
     <div className="fusee">
       <span>Retour</span>
+        <div className="carre-rouge"></div>
     </div>
   </div>
 </Link>
