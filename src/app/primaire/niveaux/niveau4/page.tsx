c469dea21dcc2505943e07119301b55b37acb8ac
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-07-07 11:48:48 -0400

    new

--- src/app/primaire/niveaux/niveau4/page.tsx
+++ src/app/primaire/niveaux/niveau4/page.tsx
@@ -59,20 +59,17 @@ export default function Niveau4() {
       <div className="grid grid-cols-2 gap-8 z-10 max-w-4xl">
         <a
           href="/primaire/niveaux/niveau4/perimetre"
-          className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-6 px-12 rounded-2xl text-3xl shadow-lg text-center flex items-center justify-center min-w-[280px] transition transform hover:scale-105"
-        >
+          className="bg-yellow-500 hover:bg-yellow-600 text-white font-bold py-6 px-12 rounded-2xl text-3xl shadow-lg text-center flex items-center justify-center min-w-[280px] transition transform hover:scale-105">
           Périmètre
         </a>
         <a
           href="/primaire/niveaux/niveau4/geometrie"
-          className="bg-red-400 hover:bg-red-500 text-white font-bold py-6 px-12 rounded-2xl text-3xl shadow-lg text-center flex items-center justify-center min-w-[280px] transition transform hover:scale-105"
-        >
+          className="bg-red-400 hover:bg-red-500 text-white font-bold py-6 px-12 rounded-2xl text-3xl shadow-lg text-center flex items-center justify-center min-w-[280px] transition transform hover:scale-105">
           Géométrie
         </a>
         <a
           href="/primaire/niveaux/niveau4/aire"
-          className="col-span-2 justify-self-center bg-green-500 hover:bg-green-600 text-white font-bold py-6 px-12 rounded-2xl text-3xl shadow-lg text-center flex items-center justify-center min-w-[280px] transition transform hover:scale-105"
-        >
+          className="col-span-2 justify-self-center bg-green-500 hover:bg-green-600 text-white font-bold py-6 px-12 rounded-2xl text-3xl shadow-lg text-center flex items-center justify-center min-w-[280px] transition transform hover:scale-105">
           Aire
         </a>
       </div>
--- src/app/primaire/niveaux/niveau5/exposant/n2/page.tsx
+++ src/app/primaire/niveaux/niveau5/exposant/n2/page.tsx
@@ -108,11 +108,11 @@ export default function ExponentsPractice() {
   return (
    <div className="min-h-screen flex flex-col justify-center items-center text-white relative overflow-hidden bg-[#081c34] font-fredoka">
 <Link href="/menu/apprendre"
-  className="btn-candy-border bg-blue-200 text-white font-bold py-6 px-12 rounded-xl text-3xl shadow-xl text-center flex items-center justify-center min-w-[300px] transition transform hover:scale-105">
+  className="fixed bottom-4 left-4 btn-candy-border bg-blue-200 text-white font-bold py-6 px-12 rounded-xl text-3xl shadow-xl text-center flex items-center justify-center min-w-[300px] transition transform hover:scale-105">
   Apprendre
 </Link>
 <Link href="/primaire/niveaux/niveau5"
-  className="btn-candy-border bg-blue-200 text-white font-bold py-6 px-12 rounded-xl text-3xl shadow-xl text-center flex items-center justify-center min-w-[300px] transition transform hover:scale-105">
+  className="absolute top-4 right-4 btn-candy-border bg-blue-200 text-white font-bold py-6 px-12 rounded-xl text-3xl shadow-xl text-center flex items-center justify-center min-w-[300px] transition transform hover:scale-105">
   Retour
 </Link>
 
