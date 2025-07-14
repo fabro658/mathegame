b86196bb5dff11c21269795b4e08690c101b5ed5
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-07-14 15:54:51 -0400

    new

--- src/app/mobile/secondaire_mobile/page.tsx
+++ src/app/mobile/secondaire_mobile/page.tsx
@@ -32,7 +32,7 @@ export default function Secondaire_mobile() {
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau3_mobile">
           <div className="rounded-tl-full rounded-bl-full transition-colors flex items-center justify-center bg-red-500 text-white gap-2 hover:bg-red-600 text-sm sm:text-base h-10 sm:h-12 w-full sm:w-64 px-4 sm:px-5 md:w-72 md:mr-[-36px]">
-            Notions
+            Algèbre
           </div>
         </Link>
         <Link href="/mobile/secondaire_mobile/niveaux_mobile/niveau4_mobile">
