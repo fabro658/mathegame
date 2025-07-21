c574a57f56ac23e1afa530c8cd53adf50422383b
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-07-21 14:50:09 -0400

    new

--- src/app/secondaire/niveaux/niveau4/aire/page.tsx
+++ src/app/secondaire/niveaux/niveau4/aire/page.tsx
@@ -169,7 +169,7 @@ export default function AreaByCounting() {
         Apprendre
       </Link>
       <Link
-        href="/primaire/niveaux/niveau4"
+        href="/secondaire/niveaux/niveau4"
         className="fixed top-4 right-4 bg-orange-500 text-white py-3 px-8 rounded font-bold z-50"
       >
         Retour
