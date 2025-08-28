d7c6158494e163a686954cb2e686dd11d05abce4
felix <felix-antoine.brodeur.1@ulaval.ca>
2025-08-28 15:47:34 -0400

    Test N4

--- src/app/mobile/primaire_mobile/niveaux_mobile/niveau4_mobile/page.tsx
+++ src/app/mobile/primaire_mobile/niveaux_mobile/niveau4_mobile/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Page() {
   return (
-    <div className="h-screen overflow-y-auto flex flex-col items-center bg-blue-100 text-gray-900 p-4 relative">
+    <div className="fixed inset-0 overflow-y-auto flex flex-col items-center bg-blue-100 text-gray-900 p-4 touch-pan-y">
       {/* Bouton de retour en haut à droite */}
       <Link
         href="/mobile/primaire_mobile"
@@ -18,7 +18,7 @@ export default function Page() {
       </h1>
 
       {/* Boutons (scrollables avec la page) */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8 w-full justify-items-center mb-12">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 gap-8 w-full justify-items-center mb-12 pb-24">
         <a
           href="/mobile/primaire_mobile/niveaux_mobile/niveau4_mobile/aire_mobile"
           className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-6 px-12 rounded-lg text-3xl shadow-lg text-center flex items-center justify-center w-[300px] leading-snug"
