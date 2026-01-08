ceabe92f2d11ebdb9fee3c798eefa7d5d835b517
felix <felix-antoine.brodeur.1@ulaval.ca>
2026-01-07 19:24:26 -0500

    new

--- src/components/HeaderSwitcher.tsx
+++ src/components/HeaderSwitcher.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { usePathname } from "next/navigation";
+import HeaderAuth from "@/components/HeaderAuth";
 import Navbar from "@/components/Navbar";
 
 export default function HeaderSwitcher() {
   const pathname = usePathname();
   const isHome = pathname === "/";
 
-  // Accueil: on n'affiche PAS le navbar (donc l'accueil ne change pas)
-  if (isHome) return null;
+  // Accueil: on garde les boutons Connexion/Inscription (ton header existant)
+  if (isHome) return <HeaderAuth basePath="" />;
 
   // Autres pages: hamburger/menu
   return <Navbar />;
